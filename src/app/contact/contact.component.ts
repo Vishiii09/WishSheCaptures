3f4196ea9457260b0e41d620224679320b394c0d
VishiSharma <vishisharma@fico.com>
2024-05-19 17:09:19 +0530

    contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,11 +8,29 @@ import { Location } from '@angular/common';
   styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
+  isOpen: boolean = false;
+  contactForm: FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', Validators.required),
+  });
+
   constructor(private location: Location) {}
 
   ngOnInit(): void {}
 
   close() {
+    this.isOpen = false;
     this.location.back();
+  }
+
+  submitForm() {
+    if (this.contactForm.valid) {
+      // Handle form submission
+      console.log('Form Submitted', this.contactForm.value);
+    } else {
+      // Mark all fields as touched to trigger validation
+      this.contactForm.markAllAsTouched();
+    }
   }
 }
